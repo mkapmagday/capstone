50a7c44bcc7e476cd19c7547743353f6dec2a85d
Kmagday <kmagday9@gmail.com>
2022-11-07 18:20:23 +0800

    update

--- resources/views/layouts/anavigation.blade.php
+++ resources/views/layouts/anavigation.blade.php
@@ -114,7 +114,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{route('admindashboard.index')}}" class="nav-link">
+                    <a href="{{route('admindashboard')}}" class="nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
