401fe262be6c6b6386ffc250ed2cff0c0508d0f5
Kmagday <kmagday9@gmail.com>
2022-11-19 18:15:07 +0800

    update

--- resources/views/layouts/anavigation.blade.php
+++ resources/views/layouts/anavigation.blade.php
@@ -48,7 +48,15 @@
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge"></span>
+                <span class="badge badge-warning navbar-badge">
+                    @forelse(App\Models\User::find(2)->notifications as $notif)
+                    @if($loop->last)
+                        {{$loop->last}}
+                    @endif
+                    @empty
+                        0
+                    @endforelse
+                </span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <span class="dropdown-item dropdown-header"> Notifications</span>
