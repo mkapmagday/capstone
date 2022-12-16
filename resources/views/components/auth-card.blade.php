ebfd4575a1412077a7e85d5137cb2e7b9b78730b
PAOLORODIL <mpcrodil@tip.edu.ph>
2022-12-16 12:43:50 +0800

    Registration.

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,11 +1,14 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" 
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
     style="background-image: url('https://i.ibb.co/ss3RXh3/Login.jpg'); background-repeat: no-repeat; 
     background-attachment: fixed; background-size: cover;">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-image: linear-gradient(white, #7fa046);">
-        {{ $slot }}
+    <div class=" sm: mt-3 px-3 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-image: linear-gradient(white, #7fa046); width:650px;">
+    <center>
+    {{ $slot }}
+    </center>
+        
     </div>
 </div>
